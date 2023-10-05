60c43f9b3a351832dc836c4ce23e5d0b8df201f3
Taktak Alaa <alataktak11@gmail.com>
2023-10-05 14:10:33 +0000

    [Task]integration Swagger

--- src/time-log/time-log.controller.ts
+++ src/time-log/time-log.controller.ts
@@ -1,24 +1,28 @@
-import { Controller,Get,Post,Put,Param } from '@nestjs/common';
+import { Controller, Get, Post, Put, Param } from '@nestjs/common';
 import { TimeLogService } from './time-log.service';
+import { ApiTags, ApiResponse, ApiParam } from '@nestjs/swagger'; 
 
-@Controller('time-log')
+@Controller('') 
+@ApiTags('Time Triker') 
 export class TimeLogController {
     constructor(private readonly timeLogService: TimeLogService) {}
 
-  @Get()
-  async findAll() {
-    return this.timeLogService.findAll();
-  }
+    @Get()
+    @ApiResponse({ status: 200, description: 'Get all time logs' })
+    async findAll() {
+        return this.timeLogService.findAll();
+    }
 
-  @Post('start')
-  async startNewTimeLog() {
-    return this.timeLogService.startNew();
-  }
+    @Post('start')
+    @ApiResponse({ status: 201, description: 'Start a new time log' })
+    async startNewTimeLog() {
+        return this.timeLogService.startNew();
+    }
 
-
-  @Put('stop/:id')
-  async stopTimeLog(@Param('id') id: number) {
-    return this.timeLogService.stopTimeLog(id);
-  }
-
+    @Put('stop/:id')
+    @ApiParam({ name: 'id', description: 'Time log ID' })
+    @ApiResponse({ status: 200, description: 'Stop a time log by ID' })
+    async stopTimeLog(@Param('id') id: number) {
+        return this.timeLogService.stopTimeLog(id);
+    }
 }
